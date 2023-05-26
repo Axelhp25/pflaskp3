8f26d7dec3bda353a9ddba5dbf5c303eaff89d4e
Axelhp <axel.camaleon25perez@gmail.com>
2023-05-25 18:33:16 -0600

    primer

--- conexion.py
+++ conexion.py
@@ -5,4 +5,4 @@ def obtener_conexion():
     return pymysql.connect(host='us-cdbr-east-06.cleardb.net',
                                 user='b42ce11279450b',
                                 password='8e63aaf9',
-                                db='heroku_4cf2e481dd1ef90')
+                                db='heroku_4cf2e481dd1ef90',port=3306)
